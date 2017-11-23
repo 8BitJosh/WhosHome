21bd71b0fa0e85ed9cf2190d5e35bfbef42c0445
Unknown <will@rudi.org.uk>
2017-11-23 21:56:15 +0000

    nmap now uses xml parsing

--- main/main.py
+++ main/main.py
@@ -19,7 +19,7 @@ Users = {}
 if not os.path.isfile('Users.json'):
     with open('Users.json', 'w') as file:
         json.dump({}, file)
-    
+
 with open('Users.json', 'r') as file:
     Users = json.load(file)
 
@@ -54,32 +54,46 @@ def saveFile():
         json.dump(Users, file)
 
 
-async def updateNmap(): 
+async def updateNmap():
     global Users
     await asyncio.sleep(20)
-    
-    while True:
-        tempUsers = Users
-        # get nmap data
-        y = subprocess.run(['sudo', 'nmap', '-sn', ipRange], stdout=subprocess.PIPE)
-        output = str(y.stdout)
-        timeNow = datetime.now().strftime("[%d/%m/%y %H:%M:%S]")
-        print('Scan Run at ' + timeNow)
-        # process nmap data
-        vals = output.split('\\n')
 
+    while True:
+        p = subprocess.Popen(['sudo','nmap','-oX','-','-sn','192.168.0.0/24'],
+                            bufsize=10000,stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (temp_xml,temp_err) = p.communicate()
+        temp_json=bf.data(fromstring(temp_xml))
+
+        tempUsers = Users
+        timeNow = datetime.now().strftime("[%d/%m/%y %H:%M:%S]")
+        print('Scan run at {} in {} seconds, hosts up: {}'.format(timeNow,
+                            temp_json['nmaprun']['runstats']['finished']['@elapsed'],
+                            temp_json['nmaprun']['runstats']['hosts']['@up']))
         for key in tempUsers:
             tempUsers[key]['online'] = 0
 
-        i = 0
-        while 'Starting' not in vals[i]:
-            i = i+1
-
-        while i+3 < len(vals):
-            i = i+1
-            ip = vals[i][21:]
-            i = i+2
-            mac = vals[i][13:30]
+        for y in range(0,temp_json['nmaprun']['runstats']['hosts']['@up']):
+            mac = "none"
+            ip = "none"
+            ipv6 = "none"
+            if len(temp_json['nmaprun']['host'][y]['hostnames']) > 0:
+                hostname = temp_json['nmaprun']['host'][y]['hostnames']['hostname']['@name']
+            else:
+                hostname = "none"
+            host_state = temp_json['nmaprun']['host'][y]['status']['@state']
+            if type(temp_json['nmaprun']['host'][y]['address']) == list:
+                for x in range(0,len(temp_json['nmaprun']['host'][y]['address'])):
+                    temp_addr = temp_json['nmaprun']['host'][y]['address'][x]['@addr']
+                    temp_addr_type = temp_json['nmaprun']['host'][y]['address'][x]['@addrtype']
+                    if temp_addr_type == "ipv4":
+                        ip = temp_addr
+                    elif temp_addr_type == "ipv6":
+                        ipv6 = temp_addr
+                    elif temp_addr_type == "mac":
+                        mac = temp_addr
+            else:
+                continue
 
             if mac not in tempUsers:
                 tempUsers[mac] = {}
@@ -102,8 +116,3 @@ loop.create_task(updateNmap())
 app.router.add_get('/', index)
 app.router.add_static('/static/', path=str('./main/static'), name='static')
 web.run_app(app, port=8080)
-
-
-
-
-
